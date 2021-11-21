9265e08ee0dd10935e55edfa1ea6b6bd3c22bdf2
Matt-deve <74759492+Matt-deve@users.noreply.github.com>
2021-11-21 17:13:46 +0100

    Added descriptions

--- homeScript.js
+++ homeScript.js
@@ -1,11 +1,20 @@
 var projectList = [
-    { title: "NX-Autolearn", href: "https://matt-deve.github.io/NX-autolearn/"},
-    { title: "Crosswords", href: ""}
+    { title: "NX-Autolearn", href: "https://matt-deve.github.io/NX-autolearn/", 
+    description: "Project to simulate a computer generated opponent who 'learns' to play noughts and crosses."},
+    { title: "Crosswords", href: "", 
+    description: "Crossword puzzle maker tool and user interaction pages."}
 ]
 
 function afterPageLoad() {
     projectList.forEach(element => {
-        $(projectsContainer).append($("<h2></h2>").text(element.title).attr("href",element.href))
-    });
-    
+        $(projectsContainer).append($("<div></div>")
+            .append($("<a></a>")
+                .text(element.title)
+                .attr("href",element.href)
+            )
+            .append($("<br>"))
+            .append($("<p></p>")
+                .text(element.description))
+        )
+    });  
 }
